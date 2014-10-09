a4504740e558aa812dd48a5bf8afc1d1e68e8189
HJWSoftware <wanbabb1@126.com>
2014-10-09 10:33:20 +0800

    test

---
+++ source/server/nourriture-web/src/test/java/edu/bjtu/nourriture_web/ExampleTestCase.java
@@ -0,0 +1,14 @@
+package edu.bjtu.nourriture_web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTestCase {
+
+	@Test
+	public void test() {
+		assertEquals("employee_info", "employee_info1"); 
+	}
+
+}
