74a1cb7c6334618d3c56d26cdb16f44e2b1332a9
HJWSoftware <wanbabb1@126.com>
2014-10-10 18:29:05 +0800

    androidtest

--- source/android/nourriture-android-test/src/edu/bjtu/nourriture_android_test/SampleTest.java
+++
@@ -1,9 +0,0 @@
-package edu.bjtu.nourriture_android_test;
-
-import android.test.InstrumentationTestCase;
-
-public class SampleTest extends InstrumentationTestCase{
-	public void testAdd(){
-		assertEquals(1, 2);
-	}
-}
