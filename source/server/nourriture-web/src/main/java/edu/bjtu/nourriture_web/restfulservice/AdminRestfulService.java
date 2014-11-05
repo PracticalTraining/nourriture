542ca41405e6935b49adae4c0d2de25c483928da
HJWSoftware <wanbabb1@126.com>
2014-11-05 21:16:30 +0800

    update restfulservice

--- source/server/nourriture-web/src/main/java/edu/bjtu/nourriture_web/restfulservice/AdminRestfulService.java
+++ source/server/nourriture-web/src/main/java/edu/bjtu/nourriture_web/restfulservice/AdminRestfulService.java
@@ -2,7 +2,17 @@ package edu.bjtu.nourriture_web.restfulservice;
 
 import javax.ws.rs.Path;
 
+import edu.bjtu.nourriture_web.idao.IAdminDao;
+
 @Path("admin")
 public class AdminRestfulService {
+	private IAdminDao adminDao;
 
+	public IAdminDao getAdminDao() {
+		return adminDao;
+	}
+
+	public void setAdminDao(IAdminDao adminDao) {
+		this.adminDao = adminDao;
+	}
 }
