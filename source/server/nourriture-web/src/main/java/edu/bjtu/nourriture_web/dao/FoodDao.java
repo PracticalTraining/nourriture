7ff88917a140311932d9d7f19488204c2c606751
qstar <davesla2012@hotmail.com>
2014-11-10 17:16:29 +0800

    manufacturer part

--- source/server/nourriture-web/src/main/java/edu/bjtu/nourriture_web/dao/FoodDao.java
+++ source/server/nourriture-web/src/main/java/edu/bjtu/nourriture_web/dao/FoodDao.java
@@ -1,9 +1,17 @@
 package edu.bjtu.nourriture_web.dao;
 
+import java.util.List;
+
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
+import edu.bjtu.nourriture_web.bean.Food;
 import edu.bjtu.nourriture_web.idao.IFoodDao;
 
 public class FoodDao extends HibernateDaoSupport implements IFoodDao {
-
+	/** search the food according to manufacturerId **/
+	public List<Food> getByManufacturerId(int manufacturerId) {
+		List<Food> list = getHibernateTemplate().find(
+				"from food where manufacturerId=?", manufacturerId);
+		return list;
+	}
 }
