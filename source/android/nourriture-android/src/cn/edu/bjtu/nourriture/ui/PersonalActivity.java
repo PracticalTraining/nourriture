cd8d01ec75c0a5d2f26cd803d7467e76e9b1c833
dong <dong@dong-Inspiron-N4010>
2014-12-18 11:16:47 +0800

    update the customer info and password

--- source/android/nourriture-android/src/cn/edu/bjtu/nourriture/ui/PersonalActivity.java
+++ source/android/nourriture-android/src/cn/edu/bjtu/nourriture/ui/PersonalActivity.java
@@ -18,18 +18,18 @@ import cn.edu.bjtu.nourriture.widgets.CustomScrollView;
 
 import com.lidroid.xutils.util.LogUtils;
 
-
 public class PersonalActivity extends BaseActivity implements OnClickListener {
 
 	private ImageView mBackgroundImageView = null;
-	private Button mLoginButton,mMoreButton,mExitButton;
+	private Button mLoginButton, mMoreButton, mExitButton;
 	private CustomScrollView mScrollView = null;
-	private Intent mIntent=null;
+	private Intent mIntent = null;
 	private ExitView exit;
-	private LinearLayout Ly_login,Ly_Other;
-	private RelativeLayout Ly_personalInfo,ly_changePwd,ly_changeInfo,ly_setLang,ly_add_food,ly_add_recipe;
-	private TextView username,jobtitle;
-	private int LOGIN_CODE=100;
+	private LinearLayout Ly_login, Ly_Other;
+	private RelativeLayout Ly_personalInfo, ly_changePwd, ly_changeInfo,
+			ly_setLang, ly_add_food, ly_add_recipe;
+	private TextView username, jobtitle;
+	private int LOGIN_CODE = 100;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +39,12 @@ public class PersonalActivity extends BaseActivity implements OnClickListener {
 		findViewById();
 		initView();
 	}
-	
+
 	@Override
 	protected void onStart() {
 		LogUtils.d("onStart");
 		super.onStart();
-		if(EMobileTask.getCookie("userId") == null){
+		if (EMobileTask.getCookie("userId") == null) {
 			Ly_personalInfo.setVisibility(View.GONE);
 			Ly_login.setVisibility(View.VISIBLE);
 			mExitButton.setVisibility(View.GONE);
@@ -63,13 +63,12 @@ public class PersonalActivity extends BaseActivity implements OnClickListener {
 		mBackgroundImageView = (ImageView) findViewById(R.id.personal_background_image);
 		mLoginButton = (Button) findViewById(R.id.personal_login_button);
 		mScrollView = (CustomScrollView) findViewById(R.id.personal_scrollView);
-		mExitButton=(Button)this.findViewById(R.id.personal_exit);
-		
-		
-		Ly_login=(LinearLayout)findViewById(R.id.login);
-		Ly_personalInfo=(RelativeLayout)findViewById(R.id.personal);
-		Ly_Other=(LinearLayout)findViewById(R.id.other_layout);
-		username=(TextView)findViewById(R.id.username);
+		mExitButton = (Button) this.findViewById(R.id.personal_exit);
+
+		Ly_login = (LinearLayout) findViewById(R.id.login);
+		Ly_personalInfo = (RelativeLayout) findViewById(R.id.personal);
+		Ly_Other = (LinearLayout) findViewById(R.id.other_layout);
+		username = (TextView) findViewById(R.id.username);
 		jobtitle = (TextView) findViewById(R.id.jobtitle);
 		ly_changePwd = (RelativeLayout) findViewById(R.id.relativelayout_change_pwd);
 		ly_changeInfo = (RelativeLayout) findViewById(R.id.relativelayout_change_info);
@@ -82,7 +81,7 @@ public class PersonalActivity extends BaseActivity implements OnClickListener {
 	protected void initView() {
 		// TODO Auto-generated method stub
 		mScrollView.setImageView(mBackgroundImageView);
-		
+
 		mLoginButton.setOnClickListener(this);
 		mExitButton.setOnClickListener(this);
 		ly_changePwd.setOnClickListener(this);
@@ -94,59 +93,61 @@ public class PersonalActivity extends BaseActivity implements OnClickListener {
 
 	@Override
 	public void onClick(View v) {
-		//CommonTools.showShortToast(PersonalActivity.this, "稍后开放");
+		// CommonTools.showShortToast(PersonalActivity.this, "稍后开放");
 		switch (v.getId()) {
 		case R.id.personal_login_button:
-			mIntent=new Intent(PersonalActivity.this, LoginActivity.class);
-			
+			mIntent = new Intent(PersonalActivity.this, LoginActivity.class);
+
 			startActivityForResult(mIntent, LOGIN_CODE);
 			break;
-			
+
 		case R.id.personal_exit:
-			
-			//实例化SelectPicPopupWindow
+
+			// 实例化SelectPicPopupWindow
 			exit = new ExitView(PersonalActivity.this, itemsOnClick);
-			//显示窗口
-			exit.showAtLocation(PersonalActivity.this.findViewById(R.id.layout_personal), Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0); //设置layout在PopupWindow中显示的位置
-			
-			
+			// 显示窗口
+			exit.showAtLocation(
+					PersonalActivity.this.findViewById(R.id.layout_personal),
+					Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0); // 设置layout在PopupWindow中显示的位置
+
 			break;
-			
+
 		case R.id.relativelayout_change_pwd:
-			if(EMobileTask.getCookie("userId") == null){
-				startActivity(new Intent(this,LoginActivity.class));
+			if (EMobileTask.getCookie("userId") == null) {
+				startActivity(new Intent(this, LoginActivity.class));
 			} else {
-				startActivity(new Intent(this,ChangePwdActivity.class));
+				startActivity(new Intent(this, ChangePwdActivity.class));
 			}
 			break;
-			
+
 		case R.id.relativelayout_set_lang:
-			startActivity(new Intent(this,SetLangActivity.class));
+			startActivity(new Intent(this, SetLangActivity.class));
 			break;
-			
+
 		case R.id.relativelayout_change_info:
-			if(EMobileTask.getCookie("userId") == null){
-				startActivity(new Intent(this,LoginActivity.class));
-			} else if (EMobileTask.getCookie("idendity").equals("普通用户")){
-				startActivity(new Intent(this,EditBormalInfoActivity.class));
+			String id = EMobileTask.getCookie("userId");
+			if (id == null) {
+				startActivity(new Intent(this, LoginActivity.class));
+			} else if (EMobileTask.getCookie("idendity").equals("普通用户")) {
+				startActivity(new Intent(this, EditBormalInfoActivity.class));
 			} else {
-				startActivity(new Intent(this,EditManuInfoActivity.class));
+				startActivity(new Intent(this, EditManuInfoActivity.class));
 			}
 			break;
 		case R.id.relativelayout_add_food:
-			if(EMobileTask.getCookie("userId") == null){
-				startActivity(new Intent(this,LoginActivity.class));
-			} else if (EMobileTask.getCookie("idendity").equals("普通用户")){
+			if (EMobileTask.getCookie("userId") == null) {
+				startActivity(new Intent(this, LoginActivity.class));
+			} else if (EMobileTask.getCookie("idendity").equals("普通用户")) {
 				DisplayToast("普通用户不能添加食物");
 			} else {
-				//startActivity(new Intent(this,EditBormalInfoActivity.class));
+				// startActivity(new Intent(this,EditBormalInfoActivity.class));
 			}
 			break;
 		case R.id.relativelayout_add_recipe:
-			if(EMobileTask.getCookie("userId") == null){
-				startActivity(new Intent(this,LoginActivity.class));
-			} else if (EMobileTask.getCookie("idendity").equals("普通用户")){
-				//startActivity(new Intent(this,EditBormalInfoActivity.class));
+			if (EMobileTask.getCookie("userId") == null) {
+				startActivity(new Intent(this, LoginActivity.class));
+			} else if (EMobileTask.getCookie("idendity").equals("普通用户")) {
+				// startActivity(new Intent(this,EditBormalInfoActivity.class));
 			} else {
 				DisplayToast("厂商不能添加食谱");
 			}
@@ -154,14 +155,14 @@ public class PersonalActivity extends BaseActivity implements OnClickListener {
 		default:
 			break;
 		}
-		
+
 	}
-	
-	//为弹出窗口实现监听类
-    private OnClickListener itemsOnClick = new OnClickListener(){
+
+	// 为弹出窗口实现监听类
+	private OnClickListener itemsOnClick = new OnClickListener() {
 
 		public void onClick(View v) {
-			
+
 			switch (v.getId()) {
 			case R.id.btn_exit:
 				EMobileTask.remove("userId");
@@ -179,6 +180,6 @@ public class PersonalActivity extends BaseActivity implements OnClickListener {
 				break;
 			}
 		}
-    };
-	
+	};
+
 }
