627b5a36fbd72754071a241f8d5deec39cb6c9d7
HJWSoftware <wanbabb1@126.com>
2014-12-19 15:30:15 +0800

    temp

--- source/android/nourriture-android/src/cn/edu/bjtu/nourriture/ui/SearchResultActivity.java
+++ source/android/nourriture-android/src/cn/edu/bjtu/nourriture/ui/SearchResultActivity.java
@@ -1,5 +1,8 @@
 package cn.edu.bjtu.nourriture.ui;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,13 +18,8 @@ import cn.edu.bjtu.nourriture.utils.CommonTools;
 
 public class SearchResultActivity extends BaseActivity {
 
-	private EditText mobile;
-	private String registerNum;
-	private ImageButton checkBox;
-	private Button access_password, food,recipe;
-	private CommonTools tools;
-	private boolean flag = false;
-	private Intent mIntent;
+	private Button food,recipe;
+	private Intent intent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +28,30 @@ public class SearchResultActivity extends BaseActivity {
 		setContentView(R.layout.activity_search_result);
 		findViewById();
 		initView();
+		
+		intent = getIntent();
+		String foodsStr = intent.getStringExtra("foods");
+		String recipesStr = intent.getStringExtra("recipes");
+		try {
+			JSONArray foods = new JSONArray(foodsStr);
+			JSONArray recipes = new JSONArray(recipesStr);
+			int foodNum = foods.length();
+			int recipeNum = recipes.length();
+			food.setText("食物 " + foodNum +"个搜索结果");
+			recipe.setText("食谱 " + recipeNum +"个搜索结果");
+			if(foodNum == 0){
+				food.setEnabled(false);
+			}
+			if(recipeNum == 0){
+				recipe.setEnabled(false);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+			food.setText("食物 0个搜索结果");
+			recipe.setText("食谱 0个搜索结果");
+			food.setEnabled(false);
+			recipe.setEnabled(false);
+		}
 	}
 
 	@Override
