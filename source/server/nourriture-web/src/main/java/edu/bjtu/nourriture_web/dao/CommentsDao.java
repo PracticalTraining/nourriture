e4a7070215692083ba4c12f91c10c4f012e12b30
dong <dong@dong-Inspiron-N4010>
2014-12-27 10:27:47 +0800

    udpate the comments

--- source/server/nourriture-web/src/main/java/edu/bjtu/nourriture_web/dao/CommentsDao.java
+++ source/server/nourriture-web/src/main/java/edu/bjtu/nourriture_web/dao/CommentsDao.java
@@ -14,4 +14,24 @@ public class CommentsDao extends HibernateDaoSupport implements ICommentsDao {
 				"from comments where refId=?", refId);
 		return list;
 	}
+
+	public List<Comments> getAllComments() {
+		return getHibernateTemplate().find("from comments");
+	}
+
+	public int add(Comments comments) {
+		getHibernateTemplate().save(comments);
+		return comments.getId();
+	}
+
+	// public List<Customer> searchByName(String name) {
+	// List<Customer> list = getHibernateTemplate().find(
+	// "from Customer where name like ?", "%" + name + "%");
+	// return list;
+	// }
+
+	// public void update(Customer customer) {
+	// getHibernateTemplate().saveOrUpdate(customer);
+	//
+	// }
 }
